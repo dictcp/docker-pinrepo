12772943d394a2c4fab1692c502c8da37ff7d1a3
Dick Tang <dick.tang@ymail.com>
2018-10-20 15:28:44 +0800

    inherit nameserver setting from /etc/resolv.conf

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 OPENRESTY_HOME=/usr/local/openresty
+export NAMESERVER=`cat /etc/resolv.conf | grep "nameserver" | awk '{print $2}' | tr '\n' ' '`
 
-envsubst '$AWS_ACCESS_KEY_ID $AWS_SECRET_ACCESS_KEY $S3_BUCKET $S3_REGION' > $OPENRESTY_HOME/nginx/conf/nginx.conf < $OPENRESTY_HOME/nginx/conf/nginx.conf.tpl
+envsubst '$AWS_ACCESS_KEY_ID $AWS_SECRET_ACCESS_KEY $S3_BUCKET $S3_REGION $NAMESERVER' > $OPENRESTY_HOME/nginx/conf/nginx.conf < $OPENRESTY_HOME/nginx/conf/nginx.conf.tpl
 
 $OPENRESTY_HOME/bin/openresty -g "daemon off;"
